678f1dbe2465fe123a3855b3c2f578be09d6684b
xabixab <xabixab@gmail.com>
2017-01-28 20:37:18 +0100

    zero commti

--- certbot-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-wget https://dl.eff.org/certbot-auto -O /usr/bin/certbot-auto
-chmod +x /usr/bin/certbot-auto
-/usr/bin/certbot-auto
